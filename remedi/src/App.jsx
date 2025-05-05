0b89b7cd375ba13afcb7af7eb66c98ae90e18e96
aanchal21 <aanchalbhargava2005@gmail.com>
2025-05-05 10:26:29 +0530

    added the api

--- remedi/src/App.jsx
+++ remedi/src/App.jsx
@@ -1,34 +1,17 @@
 import React , {useState} from 'react'
 import SideBar from './components/Sidebar'
 import Main from './components/Main'
-import axios from 'axios';
+
 
 
 const App = () => {
-  const [question , setQuestion] =useState("");
-  const [answer , setAnswer] = useState("");
-
-async function generateAnswer(){
-  setAnswer("loading....")
-  const response = await axios({
-    url:"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyBhYBDQ3sc03XWGy_EjVRY3gsvZTIpRUhw",
-    method:"post" ,
-    data: {
-      contents: [{
-        parts:[{text: question }]
-        },],
-       },
-  });
-  setAnswer(response['data']['candidates'][0]['content']['parts'][0]['text']);
-}
+ 
 
   return (
     <>
       <SideBar/>
       <Main/>
-      <textarea value={question} onChange={(e)=> setQuestion(e.target.value)}></textarea>
-      <button onClick={generateAnswer}>Generate answer</button>
-      <p>{answer}</p>
+     
     </>
   )
 }
--- remedi/src/components/Main.jsx
+++ remedi/src/components/Main.jsx
@@ -8,7 +8,34 @@ import card4_icon from './card4_icon.png'
 import add_image_icon from './add_image_icon.png'
 import voice_icon from './voice_icon.png'
 import send_icon from './send_icon.png'
+import axios from 'axios';
+
 function Main(){
+        
+    const [question , setQuestion] =useState("");
+    const [answer , setAnswer] = useState("");
+  
+  async function generateAnswer(){
+    try{
+    setAnswer("loading....")
+    const response = await axios({
+      url:"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyBhYBDQ3sc03XWGy_EjVRY3gsvZTIpRUhw",
+      method:"post" ,
+      data: {
+        contents: [{
+          parts:[{text: question }]
+          },],
+         },
+    });
+    setAnswer(response['data']['candidates'][0]['content']['parts'][0]['text']);
+  }catch (error) {
+    console.error("Error fetching answer:", error);
+    setAnswer("Sorry, something went wrong.");}
+  }
+        
+
+
+
      return(
         <div className="main">
              <div className="nav">
@@ -17,10 +44,12 @@ function Main(){
              </div>
 
              <div className="main-container">
+             {question=="" ? <div className="main-top">
                 <div className="greet">
                     <p><span>Hello,</span></p>
                     <p>How can I help you today?</p>
                 </div>
+                 
                 <div className="cards">
 
                     <div className="card">
@@ -44,13 +73,14 @@ function Main(){
                     </div>
 
                 </div>
+                </div> : <p>{answer}</p> }
                 <div className="main-bottom">
                     <div className="search-box">
-                        <input type="text" placeholder="Enter a prompt here"/>
+                        <input type="text" value={question} onChange={(e)=> setQuestion(e.target.value)} placeholder="Enter a prompt here"/>
                         <div>
                             <img src={add_image_icon} alt="" />
                             <img src={voice_icon} alt="" />
-                            <img src={send_icon} alt="" />
+                            <img src={send_icon} alt=""   onClick={generateAnswer} />
                         </div>
                     </div>
                     <p className="bottom-info">
